578b320af68c2eeeb1c78b3531747cbf1f63cc5a
killcerr <kill_cerr@outlook.com>
2026-03-03 09:29:36 +0800

    Add bedrock Encryption Helper

---
+++ encryption-helper/bedrock/src/dllmain.cpp
@@ -0,0 +1,53 @@
+#include "MinHook.h"
+#include "libhat.hpp" // IWYU pragma: keep
+#include <cstdio>
+
+struct EVP_CIPHER_CTX;
+struct EVP_CIPHER;
+struct ENGINE;
+
+int (*ORIGINAL_EVP_EncryptInit_ex)(EVP_CIPHER_CTX *ctx, const EVP_CIPHER *type,
+                                   ENGINE *impl, const unsigned char *key,
+                                   const unsigned char *iv, int enc);
+
+int EVP_EncryptInit_ex(EVP_CIPHER_CTX *ctx, const EVP_CIPHER *type,
+                       ENGINE *impl, const unsigned char *key,
+                       const unsigned char *iv, int enc) {
+  if (key) {
+    for (auto i = 0uz; i < 32; i++) {
+      std::printf("%s ", std::to_string(key[i]).c_str());
+    }
+    std::puts("");
+  } else {
+    std::puts("key is NULL");
+  }
+
+  return ORIGINAL_EVP_EncryptInit_ex(ctx, type, impl, key, iv, enc);
+}
+
+void init() {
+  using namespace hat::literals::signature_literals;
+  auto mc = hat::process::get_process_module();
+  MH_Initialize();
+  if (auto target =
+          hat::find_pattern(
+              mc.get_module_data(),
+              "48 89 6C 24 ? 48 89 74 24 ? 48 89 7C 24 ? 41 54 41 56 41 57 B8 ? ? ? ? E8 ? ? ? ? 48 2B E0 8B 44 24"_sig)
+              .get()) {
+    MH_CreateHook(target, reinterpret_cast<void *>(&EVP_EncryptInit_ex),
+                  reinterpret_cast<void **>(&ORIGINAL_EVP_EncryptInit_ex));
+    std::printf("hooked EVP_EncryptInit_ex() at %p\n", target);
+  } else {
+    std::puts("fail to look up pattern for EVP_EncryptInit_ex()");
+  }
+  MH_EnableHook(MH_ALL_HOOKS);
+}
+
+BOOL WINAPI DllMain(HINSTANCE, DWORD reason, LPVOID) {
+  switch (reason) {
+  case DLL_PROCESS_ATTACH:
+    init();
+    break;
+  }
+  return TRUE;
+}
